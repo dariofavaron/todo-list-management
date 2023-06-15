d84568b70f57958d27e1448dee1c5c9fe199eccf
Dario <dario.favaron@gmail.com>
2023-06-15 17:40:18 +0200

    fix

--- pages/actions/actions.py
+++ pages/actions/actions.py
@@ -21,7 +21,7 @@ def main():
         board = Dashboard()
         w = SimpleNamespace(
             dashboard=board,
-            prompt_card=prompt_card(board, 0, 0, 2, 2, minW=1, minH=1),
+            prompt_card=PromptCard(board, 0, 0, 2, 2, minW=1, minH=1),
         )
         state.w = w
 
@@ -42,29 +42,26 @@ def main():
 
 
 
----
+    # with elements("dashboard"):
 
+    #     layout = [
+    #         # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
+    #         dashboard.Item("first_item", 0, 1, 2, 2),
+    #         dashboard.Item("second_item", 0, 3, 2, 2, isDraggable=False, moved=False),
+    #         dashboard.Item("third_item", 0, 5, 2, 2, isResizable=False),
+    #     ]
 
-    with elements("dashboard"):
+    #     def handle_layout_change(updated_layout):
+    #         # You can save the layout in a file, or do anything you want with it.
+    #         # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
+    #         print(updated_layout)
 
-        layout = [
-            # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
-            dashboard.Item("first_item", 0, 1, 2, 2),
-            dashboard.Item("second_item", 0, 3, 2, 2, isDraggable=False, moved=False),
-            dashboard.Item("third_item", 0, 5, 2, 2, isResizable=False),
-        ]
+    #     with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+    #         mui.Paper("First item", key="first_item")
+    #         mui.Paper("Second item (cannot drag)", key="second_item")
+    #         mui.Paper("Third item (cannot resize)", key="third_item")
 
-        def handle_layout_change(updated_layout):
-            # You can save the layout in a file, or do anything you want with it.
-            # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
-            print(updated_layout)
-
-        with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
-            mui.Paper("First item", key="first_item")
-            mui.Paper("Second item (cannot drag)", key="second_item")
-            mui.Paper("Third item (cannot resize)", key="third_item")
-
-    prompt_card()
+    # prompt_card()
 
 
 
